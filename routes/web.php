d0fbf77840f56df8b6c3316cb9495829e5d0ca7d
Xeejay-25 <cjcefren00243@usep.edu.ph>
2024-09-24 16:04:09 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Staff_dashboard;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('staff/home', [Staff_dashboard::class, 'home']);
+Route::get('staff/add', [Staff_dashboard::class, 'add']);
+Route::get('staff/list', [Staff_dashboard::class, 'list']);
+
+Route::get('staff/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+   
+});
+
+require __DIR__.'/auth.php';
