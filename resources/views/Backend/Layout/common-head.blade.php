94dc663f525f5945e5810af29f59f5af1a201915
theFeated <jvpdionsay01667@usep.edu.ph>
2024-10-13 23:44:10 +0800

    Updated google analytics

--- resources/views/Backend/Layout/common-head.blade.php
+++ resources/views/Backend/Layout/common-head.blade.php
@@ -40,3 +40,15 @@
       background-color: green;
   }
 </style>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2VC6FKHTT6"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-2VC6FKHTT6');
+    </script>
+    <body class="g-sidenav-show  bg-gray-100">
+        
